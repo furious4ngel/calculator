581397e011a3fe13c6958645583d298d89ffc2ce
t4rantino <stephanie.anoruigwe@gmail.com>
2022-03-25 08:19:40 -0700

    Define add function

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+  let result = Number(a) + Number(b);
+  let rounded = Number(result.toFixed(3));
+
+  if (result >= 1000000) {
+    result = result.toExponential(2);
+  } else if (result !== rounded) {
+    result = rounded;
+  }
+  return result;
+}
