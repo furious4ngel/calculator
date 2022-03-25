fc9e78f2fe724e04ab1f8da1912552fc3fc6309c
t4rantino <stephanie.anoruigwe@gmail.com>
2022-03-25 08:28:43 -0700

    Define subtract function

--- js/script.js
+++ js/script.js
@@ -9,3 +9,15 @@ function add(a, b) {
   }
   return result;
 }
+
+function subtract(a, b) {
+  let result = a - b;
+  let rounded = Number(result.toFixed(3));
+  
+  if (result >= 1000000) {
+    result = result.toExponential(2);
+  } else if (result !== rounded) {
+    result = rounded;
+  }
+  return result;
+}
