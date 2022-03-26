b6217f1e10b67635ea3c8b16a8830099bcbe29ec
t4rantino <stephanie.anoruigwe@gmail.com>
2022-03-25 18:53:47 -0700

    Define operate function

--- js/script.js
+++ js/script.js
@@ -27,3 +27,29 @@ function divide(a, b) {
   const ERROR = 'ERR: DIVIDE BY 0';
   return (b === '0') ? ERROR : formatResult(a / b);
 }
+
+function operate(operator, a, b) {
+  let result;
+  const operators = {
+      plus: '+',
+      minus: '-',
+      times: '×',
+      slash: '÷'
+  };
+
+  switch (operator) {
+    case operators.plus:
+      result = add(a, b);
+      break;
+    case operators.minus:
+      result = subtract(a, b);
+      break;
+    case operators.times:
+      result = multiply(a, b);
+      break;
+    case operators.slash:
+      result = divide(a, b);
+      break;
+  }
+  return result;
+}
