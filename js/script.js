6e817ec4790d2129b9bcbd535b7ab8ff3a48ac31
t4rantino <stephanie.anoruigwe@gmail.com>
2022-04-03 13:39:35 -0700

    Remove formatResult function
    I have decided not to round the return values of the math functions
    or express them as exponents because I simply want to display their
    results as is. If users want to work with very large or very small
    numbers, they can see their results in their entirety, courtesy of
    JavaScript.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,18 @@
-function formatResult(result) {
-  const THOUSANDTH = 1e-3;
-  const MILLION = 1e6;
-  let decimal = Number(result.toFixed(3));
-  let exponent = result.toExponential(2);
-
-  if (result <= THOUSANDTH || result >= MILLION) {
-    result = exponent;
-  } else if (result !== decimal) {
-    result = decimal;
-  }
-  return result;
-}
-
 function add(a, b) {
-  return formatResult(Number(a) + Number(b));
+  return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
-  return formatResult(a - b);
+  return a - b;
 }
 
 function multiply(a, b) {
-  return formatResult(a * b);
+  return a * b;
 }
 
 function divide(a, b) {
   const ERROR = 'ERR: DIVIDE BY 0';
-  return (b === '0') ? ERROR : formatResult(a / b);
+  return (b === '0') ? ERROR : a / b;
 }
 
 function operate(operator, a, b) {
